532dfd99dccb00d73ffef69099116210f0f6849c
Xpc <2536287545@qq.com>
2022-08-08 15:12:28 +0800

    second commit

--- springboot_01/src/main/java/com/cdcompany/Springboot01Application.java
+++ springboot_01/src/main/java/com/cdcompany/Springboot01Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springboot01Application {
 
 	public static void main(String[] args) {
+		//git1
 		SpringApplication.run(Springboot01Application.class, args);
 	}
 
